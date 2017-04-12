94f16f33e8eeef163ea1f1779869ded8789c6666
DmitriyParenkin <dimka-dimasik1998@yandex.ru>
2017-04-12 18:00:15 +0700

    Add function for the game player-computer

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void instriction(){
      printf("\nПравила игры: \n");
@@ -9,6 +10,49 @@ printf("-количество спичек, взятых за ход, должно быть больше 1 и меньше 11;\n")
 printf("-тот, кто делает предпоследний ход в игре, выигрывает;\n");
 printf("-тот, кто берёт последний камень из кучи, проигрывает;\n");
 printf("\n\t\t\tПриятной игры!!!\n\n");
+}
+
+int Count,Num,Player;
+bool Correct;
+int PlayerComputer(){
+      Player=1;
+  Count=100;
+  do{
+  
+    if (Player==1){
+    do{
+        printf("Ваш ход. На столе %d спичек.\n",Count);
+        printf("Сколько спичек Вы берете?\n");
+        scanf(" %d", &Num);
+        if(Num>=1&&Num<=10&&Num<=Count)
+            Correct=true;
+        else
+        {
+            printf("Неверно! Повторите ввод!\n");
+            Correct=false;
+        } 
+    }
+    while (!Correct);
+   }
+    else
+    {
+    do{
+       Num=rand()%10+1;
+      if (Num>Count)
+        Num=Count;
+      printf("Мой ход. Я взял %d спичек.\n",Num);}
+        while (!Correct);   
+    }
+    Count-=Num;
+    if (Player==1) 
+        Player=2;
+    else 
+        Player=1;
+    }
+  while (Count>0);
+  if (Player==1)
+    printf("Вы победили!");
+  else printf("Вы проиграли!");
 }
 
 int main()
