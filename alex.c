935c683c6c1c616c44f176685edf5afbb5d95413
Alexis <makarevichsanek@mail.ru>
2017-04-21 13:51:28 +0700

    Added string of menu

--- alex.c
+++ alex.c
@@ -3,16 +3,20 @@
 #include <stdbool.h>
 void table_game(){
 	printf("\t ____________________________________________________ \n");
-	printf("\t|                         Menu                       |\n");
+        printf("\t|                                                    | \n");
+	printf("\t|                        Menu                        |\n");
 	printf("\t|____________________________________________________| \n");
 	printf("\t|  |                                                 | \n");
-	printf("\t|1.|      Start game mode: human vs computer         | \n");
+	printf("\t|1.|      Start game mode: player1 vs player2.       | \n");
 	printf("\t|__|_________________________________________________|\n");
 	printf("\t|  |                                                 | \n");
-	printf("\t|2.|     Start game mode: player1 vs player2         | \n");
+	printf("\t|2.|       Start game mode: human vs computer.       | \n");
 	printf("\t|__|_________________________________________________|\n");
 	printf("\t|  |                                                 | \n");
-	printf("\t|3.|                Exit from the game.              | \n");
+	printf("\t|3.|                  Instruction.                   | \n");
+	printf("\t|__|_________________________________________________|\n");
+        printf("\t|  |                                                 | \n");
+	printf("\t|4.|               Exit from the game.               | \n");
 	printf("\t|__|_________________________________________________|\n");
 }
 int main(){
