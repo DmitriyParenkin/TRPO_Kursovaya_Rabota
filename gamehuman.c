023df37bf4f3bea824347e6c3916bbd8100d12be
Alexis <makarevichsanek@mail.ru>
2017-04-23 23:31:32 +0700

    Issued code-style

--- gamehuman.c
+++ gamehuman.c
@@ -4,40 +4,34 @@
 int Count=100;
 int Num,Player;
 bool Correct;
-int PlayHuman(){
-  Player=1;
-  do{
-  
-    if (Player==1){
-    do{
+int PlayHuman() {
+Player=1;
+do {  
+if (Player == 1) {
+do {
        printf("Move Player1. There are %d matches on the table.\n", Count);
        printf("How many matches do you take?\n");
        scanf("%d",&Num);
-        if(Num >= 1 && Num <= 10 && Num <= Count)
+       if (Num >= 1 && Num <= 10 && Num <= Count)
             Correct = true;
-        else
-        {
+       else {
             printf("Wrong! Please try again!\n");
             Correct = false;
         } 
+    } while (!Correct);
     }
-    while (!Correct);
-   }
-    else
-    {
-       do{		
+    else {
+    do {		
        printf("Move Player2. There are %d matches on the table.\n", Count);
        printf("How many matches do you take?\n");
        scanf("%d",&Num);
-       if(Num >= 1 && Num <= 10 && Num <= Count)
+       if (Num >= 1 && Num <= 10 && Num <= Count)
             Correct = true;
-        else
-        {
+       else {
             printf("Wrong! Please try again!\n");
             Correct = false;
         }	
-    }
-	  	    while (!Correct);   
+    } while (!Correct);   
     }
     Count = Count - Num;
     if (Player == 1) 
@@ -45,13 +39,12 @@ int PlayHuman(){
     else 
         Player = 1;
     }
-  while (Count > 0);
+    while (Count > 0);
   if (Player == 2)
     printf("Won Player1!");
   else printf("Won Player2!");	
-}
-int main()
-{
+  }
+int main() {
 PlayHuman();
 system("PAUSE"); 
 return 0;
